2d30e851550deca64eedd6ee220ba29260f9c764
“Aleksandr” <smylnikov@mail.ru>
2023-03-04 22:54:42 +0300

    added task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -5,8 +5,8 @@
 
 
 
-Console.Write("Введите элементы(через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.Write("Введите любые числа(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine()!.Split()!, int.Parse!);
 int count = 0;
  
 for (int i = 0; i < arr.Length; i++)
